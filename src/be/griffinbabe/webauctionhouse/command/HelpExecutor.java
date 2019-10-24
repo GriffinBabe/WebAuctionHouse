ebb96fd37a64f542ec6b6b6e4e995eb6499bd93e
GriffinBabe <darius.couchard@hotmail.com>
2019-10-24 13:12:12 +0200

    Finished create Sign reading

---
+++ src/be/griffinbabe/webauctionhouse/command/HelpExecutor.java
@@ -0,0 +1,22 @@
+package be.griffinbabe.webauctionhouse.command;
+
+import be.griffinbabe.webauctionhouse.Main;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class HelpExecutor implements CommandExecutor {
+
+    /** Plugin main class */
+    private Main plugin;
+
+    public HelpExecutor(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        commandSender.sendMessage("You fired the helpwah command!");
+        return true;
+    }
+}
